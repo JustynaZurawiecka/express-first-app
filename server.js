bd9b3be980b14c00d984dc6504af2cfb5274ef67
Justyna Żurawiecka <justyna2106@wp.pl>
2021-01-10 17:52:16 +0100

    Add new endpoint for contact form

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ app.set('view engine', '.hbs');
 
 app.use(express.static(path.join(__dirname, '/public')));
 
+app.use(express.urlencoded({ extended: false }));
+
 app.get('/', (req, res) => {
     res.render('index');
 });
@@ -33,6 +35,17 @@ app.get('/hello/:name', (req, res) => {
     // res.send(`Hello ${req.params.name}`);
     res.render('hello', { layout: false, name: req.params.name })
 });
+
+app.post('/contact/send-message', (req, res) => {
+    const { author, sender, title, message, file } =req.body;
+
+    if(author && sender && title && message && file) {
+        res.render('contact', { isSent: true, file: file });
+    } 
+    else {
+        res.render('contact', { isError: true });
+    }
+  });
 
 app.use((req, res) => {
     res.status(404).send('404 not found...');
